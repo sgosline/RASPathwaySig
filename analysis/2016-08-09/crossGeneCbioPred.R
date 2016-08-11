1543d9f05f844b4ef464d8e1b5360f40acda1343
Sara JC Gosline <sara.gosline@sagebase.org>
2016-08-11 09:53:16 -0700

    updated

--- analysis/2016-08-09/crossGeneCbioPred.R
+++ analysis/2016-08-09/crossGeneCbioPred.R
@@ -109,6 +109,7 @@ getTcgaPredStats<-function(genelist){
       mutdata<-getDisMutationData(ct)
       exprdata<-getDisExpressionData(ct)
       compats<-intersect(colnames(mutdata),colnames(exprdata))
+      
       print(paste("Found",length(compats),'patients with expression and mutation data for',ct))
         df<-crossGenePreds(genelist,mutMatrix=mutdata[,compats],exprMatrix=exprdata[,compats],cancerType=ct,prefix='tcga')
         print(paste('Finished',ct))
